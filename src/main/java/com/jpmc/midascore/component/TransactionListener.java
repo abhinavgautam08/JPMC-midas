c3119b3568dfad32d4f8148e2f1b3ec70eb1701e
Abhinav Adarsh <abhinavgautam08@users.noreply.github.com>
2026-04-03 19:00:37 +0530

    Completed Task 3

--- src/main/java/com/jpmc/midascore/component/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/component/TransactionListener.java
@@ -1,17 +1,37 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
+import com.jpmc.midascore.entity.UserRecord;
 import com.jpmc.midascore.foundation.Transaction;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.jpmc.midascore.repository.TransactionRepository;
+import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionListener {
-    static final Logger logger = LoggerFactory.getLogger(TransactionListener.class);
+
+    private final UserRepository userRepository;
+    private final TransactionRepository transactionRepository;
+
+    public TransactionListener(UserRepository userRepository, TransactionRepository transactionRepository) {
+        this.userRepository = userRepository;
+        this.transactionRepository = transactionRepository;
+    }
 
     @KafkaListener(topics = "${general.kafka-topic}")
     public void listen(Transaction transaction) {
-        logger.info("Received transaction: {}", transaction);
+        UserRecord sender = userRepository.findById(transaction.getSenderId());
+        UserRecord recipient = userRepository.findById(transaction.getRecipientId());
+
+        if (sender == null || recipient == null) return;
+        if (sender.getBalance() < transaction.getAmount()) return;
+
+        sender.setBalance(sender.getBalance() - transaction.getAmount());
+        recipient.setBalance(recipient.getBalance() + transaction.getAmount());
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+        transactionRepository.save(new TransactionRecord(sender, recipient, transaction.getAmount()));
     }
 }
